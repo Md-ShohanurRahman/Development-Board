d4818f030b7b39fd3849cdb9dbe0b25946396e2a
Md-ShohanurRahman <shohanurrahmanibnaymizan@gmail.com>
2025-03-21 11:28:54 +0600

    add the style fot the alert meaaage

--- script.js/task.js
+++ script.js/task.js
@@ -1,4 +1,5 @@
 document.getElementById("check-complete").addEventListener("click",function(){
+    alert("Board Updated Successfully")
     assignTask("taskNumber");
     completedTaskCount("completed-task-number");
     
